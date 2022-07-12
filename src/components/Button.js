c2e3bb932a27e481e91378a0eafda371ea175242
josemourinho333 <yoo.phil92@gmail.com>
2022-07-11 17:54:28 -0700

    first commit with button component working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions";
+import classNames from 'classnames';
 
 export default function Button(props) {
-   return <></>;
-}
+
+  let buttonClass = classNames('button', 
+    props.confirm ? 'button--confirm' :
+    props.danger ? 'button--danger' :
+    ''
+  )
+
+  return (
+    <button 
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
