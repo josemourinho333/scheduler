1ae43886170c3180de4b7fd454b9e99b4c4426b6
josemourinho333 <yoo.phil92@gmail.com>
2022-07-12 18:33:32 -0700

    Everything rendering fine. Setting up API next

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,47 @@
 import React, { useState } from "react";
 import DayList from './DayList';
-// import Appointment from "components/Appointment";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
 
@@ -25,6 +64,12 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+
+  const appoints = Object.values(appointments).map((appointment) => {
+    return (
+      <Appointment key={appointment.id} {...appointment} />
+    )
+  })
 
   return (
     <main className="layout">
@@ -50,7 +95,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appoints}
+        <Appointment key='last' time='5pm' />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-
+      <Header time={props.time}/>
+      { props.interview ? <Show id={props.id} student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
     </article>
   )
 }
